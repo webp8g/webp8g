c81ce38d498e9a4fc332d6d0ec2753fc91f57428
webp8g <147353850+webp8g@users.noreply.github.com>
2023-10-20 18:36:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+let isMenuOpen = false;
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const overlay = document.querySelector('.overlay');
+
+function toggleMenu() {
+  isMenuOpen = !isMenuOpen;
+
+  if (isMenuOpen) {
+    navMenu.classList.add('active');
+    overlay.classList.add('active');
+    hamburger.classList.add('active');
+  } else {
+    navMenu.classList.remove('active');
+    overlay.classList.remove('active');
+    hamburger.classList.remove('active')
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+
+ 
+  setTimeout(() => {
+    if (scrollTop > lastScrollTop && isMenuOpen) {
+      navMenu.classList.remove('active');
+      overlay.classList.remove('active');
+      hamburger.classList.remove('active');
+      isMenuOpen = false;
+    }
+  }, 150); 
+});
+
+
+
 const svgMap = document.getElementById('svg-m');
 const svg = svgMap.querySelector('#svg1');
 
@@ -55,7 +94,13 @@ svgMap.addEventListener('mousedown', startPan);
 document.addEventListener('mousemove', pan);
 document.addEventListener('mouseup', endPan);
 
-// Handle touch events
+
 svgMap.addEventListener('touchstart', startPan);
-document.addEventListener('touchmove', pan);
+
+
+document.addEventListener('touchmove', function(event) {
+  if (isDragging) {
+    event.preventDefault();
+  });
+
 document.addEventListener('touchend', endPan);
